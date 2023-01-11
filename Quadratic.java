3aea84f8c1ed5f9cc1faf3609d1a24af0fea8361
Thatcher Auten <thatcherauten24@gmail.com>
2023-01-11 11:30:18 -0600

    second solution

--- Quadratic.java
+++ Quadratic.java
@@ -1,7 +1,7 @@
 public class Quadratic {
     public static void main(String[] args) {
         
-        System.out.println("Enter three arguments, a, b, and c, to be solved with quadratic formula where ax^2+bx+c = 0");
+        // System.out.println("Enter three arguments, a, b, and c, to be solved with quadratic formula where ax^2+bx+c = 0");
 
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
@@ -12,7 +12,8 @@ public class Quadratic {
         System.out.println("c = " + c);
 
         System.out.println("x = " + ((b*-1) + Math.sqrt(Math.pow(b, 2) - 4*a*c)/2*a));
-        //System.out.println("x = " + (b*-1) - );
+        System.out.println("OR");
+        System.out.println("x = " + ((b*-1) - Math.sqrt(Math.pow(b, 2) - 4*a*c)/2*a));
 
 
 
