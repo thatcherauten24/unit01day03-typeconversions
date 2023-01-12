1b1d51cbc019b5d510c580ec5c284780bb29d7f9
Thatcher Auten <thatcherauten24@gmail.com>
2023-01-12 11:22:15 -0600

    initial commit

--- TrigIdentity.java
+++ TrigIdentity.java
@@ -1,5 +1,6 @@
 public class TrigIdentity {
     public static void main(String[] args) {
-        
+        Double theta = Double.parseDouble(args[0]);
+        System.out.println(Math.pow(java.lang.Math.sin(theta), 2) + Math.pow(java.lang.Math.cos(theta), 2));
     }
 }
