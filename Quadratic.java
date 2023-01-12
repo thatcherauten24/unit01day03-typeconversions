a4577179595a63c47d704fec5924021a1563d9b4
Thatcher Auten <thatcherauten24@gmail.com>
2023-01-12 11:19:41 -0600

    finishing up

--- Quadratic.java
+++ Quadratic.java
@@ -1,26 +1,24 @@
 public class Quadratic {
     public static void main(String[] args) {
-        
-        // System.out.println("Enter three arguments, a, b, and c, to be solved with quadratic formula where ax^2+bx+c = 0");
 
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        int c = Integer.parseInt(args[2]);
-
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        System.out.println("c = " + c);
-
-        System.out.println("x = " + ((b*-1) + (Math.sqrt(Math.pow(b, 2) + (-4*a*c)))/2*a));
-        System.out.println("OR");
-        System.out.println("x = " + ((b*-1) - (Math.sqrt(Math.pow(b, 2) + (-4*a*c)))/2*a));
+  Double a = Double.parseDouble(args[0]);
+  Double b = Double.parseDouble(args[1]);
+  Double c = Double.parseDouble(args[2]);
+  
+  Double negb = (b*-1);  
+  Double bsq = (b*b); 
+  Double  fourac = (4*a*c);
+  Double twoa = (2*a);
+  Double sqrt = Math.sqrt(bsq-fourac);
+  
 
 
 
+  Double solution1 = ((negb + sqrt) / twoa);
+  Double solution2 = ((negb - sqrt) / twoa);
 
-
-
-
-
+  System.out.println("x = " + solution1);
+  System.out.println("OR");
+  System.out.println("x = " + solution2);
     }
 }
